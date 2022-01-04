17ba8eefab58c09e824a3e1de37924ff6a3fa820
Umarbek <umarbekweb@gmail.com>
2022-01-04 19:15:13 +0500

    some changes

--- configs.js
+++ configs.js
@@ -1,6 +1,6 @@
 let configs = {
-  BACKEND_API: "https://api.themoviedb.org/3/",
-  API_KEY: "7014e2cdb739f65a296e51932f359f53",
-  DEFAULT_IMG: `${window.origin}/assets/img/icons/img-thumbnail.svg`,
+    BACKEND_API: "https://api.themoviedb.org/3/",
+    API_KEY: "7014e2cdb739f65a296e51932f359f53",
+    DEFAULT_IMG: `${window.origin}/assets/img/icons/img-thumbnail.svg`,
 };
 export default configs;
