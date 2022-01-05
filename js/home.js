40e7a3bb0e96b9aed3ae87f6a9bbd98dcff0e2b7
Umarbek <umarbekweb@gmail.com>
2022-01-05 18:52:56 +0500

    some changes

--- js/home.js
+++ js/home.js
@@ -1,17 +1,17 @@
 import configs from "./config.js";
 const { baseUrl, apiKey, imgUrl } = configs;
 export async function fetchPopularMovies() {
-  const url = `${baseUrl}popular/?api_key=${apiKey}&language=en-US&page=1`;
-  const res = await fetch(url);
-  const data = await res.json();
-  return data;
+    const url = `${baseUrl}popular/?api_key=${apiKey}&language=en-US&page=1`;
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
 }
 
 export function displayData(data) {
-  const popularMoviesDom = document.querySelector(".popular__movies");
-  let htmlContent = "";
-  data.results.forEach((movie) => {
-    htmlContent += `<div class="card" data-id = "${movie.id} ">
+    const popularMoviesDom = document.querySelector(".popular__movies");
+    let htmlContent = "";
+    data.results.forEach((movie) => {
+        htmlContent += `<div class="card" data-id = "${movie.id} ">
       <img
         height="250"
         src="${imgUrl}${movie.poster_path}"
@@ -19,13 +19,13 @@ export function displayData(data) {
       />
       <h3>${movie.title}</h3>
       </div>`;
-  });
-  popularMoviesDom.innerHTML = htmlContent;
+    });
+    popularMoviesDom.innerHTML = htmlContent;
 }
 
 export function setCoverBg(img) {
-  const heroDom = document.querySelector(".hero");
-  heroDom.style.setProperty("--coverBg", `url(${imgUrl}${img})`);
+    const heroDom = document.querySelector(".hero");
+    heroDom.style.setProperty("--coverBg", `url(${imgUrl}${img})`);
 }
 
 // document.addEventListener("DOMContentLoaded", () => {
@@ -36,12 +36,12 @@ export function setCoverBg(img) {
 // });
 
 export function movieHandler() {
-  let movies = document.querySelectorAll(".card");
-  movies.forEach((movie) => {
-    movie.onclick = (e) => {
-      let id = e.target.parentElement.dataset.id;
-      history.pushState({ id }, "movie", "/movie.html");
-      location.reload();
-    };
-  });
+    let movies = document.querySelectorAll(".card");
+    movies.forEach((movie) => {
+        movie.onclick = (e) => {
+            let id = e.target.parentElement.dataset.id;
+            history.pushState({ id }, "movie", "/movie.html");
+            location.reload();
+        };
+    });
 }
