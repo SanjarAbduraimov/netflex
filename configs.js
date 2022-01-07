9c22dfa66be6df608c658ec2d4fa5052877f32e1
Aziza Akhmedova <azizaaxmedova28@gmail.com>
2022-01-07 18:44:28 +0500

    profile js added

--- configs.js
+++ configs.js
@@ -3,4 +3,5 @@ export default {
   IMG_URL: `https://image.tmdb.org/t/p/w500/`,
   API_KEY: "7014e2cdb739f65a296e51932f359f53",
   DEFAULT_IMG: `${window.origin}/assets/img/icons/img-thumbnail.svg`,
+  SESSION_ID: "c4b0c61ecf2f28a80cc304444956cde112d4bb56"
 };
