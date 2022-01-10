3abb89c219a313a94436db76e35731cd10e4eff6
Aziza Akhmedova <azizaaxmedova28@gmail.com>
2022-01-10 18:47:54 +0500

    profile page

--- configs.js
+++ configs.js
@@ -1,7 +1,7 @@
 export default {
   BACKEND_API: `https://api.themoviedb.org/3/`,
   IMG_URL: `https://image.tmdb.org/t/p/w500/`,
-  API_KEY: "7014e2cdb739f65a296e51932f359f53",
+  API_KEY: "684e89be3aaff7fd6d43d3d90e4aa5da",
   DEFAULT_IMG: `${window.origin}/assets/img/icons/img-thumbnail.svg`,
   SESSION_ID: "c4b0c61ecf2f28a80cc304444956cde112d4bb56"
 };
--- js/profile.js
+++ js/profile.js
@@ -1,8 +1,8 @@
 import configs from "../configs.js";
-const { API_KEY, BACKEND_API, SESSION_ID } = configs;
+const { API_KEY, BACKEND_API, SESSION_ID, IMG_URL} = configs;
 
 async function fetchDetailsData() {
-  const profileUrl = `${BACKEND_API}account?api_key=684e89be3aaff7fd6d43d3d90e4aa5da&session_id=${SESSION_ID}`;
+  const profileUrl = `${BACKEND_API}account?api_key=${API_KEY}&session_id=${SESSION_ID}`;
   const res = await fetch(profileUrl);
   const data = await res.json();
   console.log(data);
@@ -14,10 +14,15 @@ async function DisplayDetails(data) {
   const profileName = document.querySelector(".profile_name");
   profileName.innerHTML = `${data.username}`;
   let imgContent = `
-    <a href="#"> <img class="profile_pic" src="${data.avatar_path}" width="100%" height="100%" alt="" /></a>`;
+    <a href="#"> <img class="profile_pic" src="${IMG_URL}${data.avatar_path}" width="100%" height="100%" alt="" /></a>`;
     imgPath.innerHTML = imgContent;
+    console.log(data.avatar_path);
 }
 
+
+async function activating() {
+  
+}
 document.addEventListener("DOMContentLoaded", () => {
   fetchDetailsData().then((data)=>{
       DisplayDetails(data)
