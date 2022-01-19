47d9676a8446ebfa83af2c739965a5cc105b8754
Aziza Akhmedova <azizaaxmedova28@gmail.com>
2022-01-19 18:35:02 +0500

    profile page fix

--- configs.js
+++ configs.js
@@ -1,7 +1,7 @@
 export default {
   BACKEND_API: `https://api.themoviedb.org/3/`,
   IMG_URL: `https://image.tmdb.org/t/p/w500/`,
-  API_KEY: "7014e2cdb739f65a296e51932f359f53",
+  API_KEY: "684e89be3aaff7fd6d43d3d90e4aa5da",
   DEFAULT_IMG: `${window.origin}/assets/img/icons/img-thumbnail.svg`,
-  SESSION_ID: "6bd6bfc00b2ea6cd0d24b457d57e60eec394652a"
+  SESSION_ID: "f5bd297f3b8e100829f18a0ba6689780ec138f92"
 };
